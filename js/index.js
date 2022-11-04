f6d02893f7b0020cd6961c7f25ae8187aa416ead
martin201285 <martin.marron@outlook.com>
2022-11-04 12:47:01 -0300

    FEAT - Se da funcionalidad al up button con javascript

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function goUp(pxScreen){
+    window.addEventListener('scroll', () => {
+        var scroll  = document.documentElement.scrollTop;
+        var upButton = document.getElementById('upButton');
+
+        if(scroll > pxScreen){
+            upButton.style.display = 'block';
+        } else {
+            upButton.style.display = 'none';
+        }
+    })
+}
+
+goUp(75);
